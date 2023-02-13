d010376cbf8f4533f42043f9a190a87ea5c833cf
WANSU-JOO <code.wsjoo@gmail.com>
2023-02-14 00:10:35 +0900

    Navbar 로고 적용

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar toggleable="lg" type="danger" variant="info">
+      <b-navbar-brand href="#">
+        <img
+          :src="require('@/assets/images/wsjoo-low-resolution-logo.png')"
+          alt="Kitten"
+          width="150"
+        >
+      </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item href="/about" disabled>Disabled</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
