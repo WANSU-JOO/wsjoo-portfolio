3c53f4d873d43757fdb3f2ef3b59fc5ecd2cc067
WANSU-JOO <code.wsjoo@gmail.com>
2023-02-13 22:33:41 +0900

    Bootstrap-vue-3 플러그인 추가

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+// Bootstrap-vue-3
+import BootstrapVue3 from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
 import { createApp } from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(BootstrapVue3)
+  .mount('#app');
---
+++ src/views/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <b-card>
+      <b-card-body>
+        <b-button>test</b-button>
+      </b-card-body>
+    </b-card>
+  </div>
+</template>
+<script setup lang="ts">
+import { BCard } from 'bootstrap-vue-3';
+</script>
