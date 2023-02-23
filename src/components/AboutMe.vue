2d6698b4c7d4caf754561960d67a5ca27dff109b
WANSU-JOO <code.wsjoo@gmail.com>
2023-02-23 23:49:49 +0900

    AboutMe (contents) scss, 내용 추가

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <a id="title">&#x1F4BB; About Me</a>
+    <br>
+    <a id="contents">안녕하세요.</a>
+    <br>
+    <a id="contents">3년 차 Frontend 개발자 주완수입니다.</a>
+    <br>
+    <a id="contents">새로운 Skill을 탐구해보고 끄적여보는 것을 좋아합니다.</a>
 
   </div>
 </template>
