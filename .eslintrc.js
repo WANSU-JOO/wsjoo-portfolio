99fb6939a1ea9cc1ebef5883a1ce07f01c1a8358
WANSU-JOO <code.wsjoo@gmail.com>
2023-02-27 23:00:26 +0900

    Project.vue 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': ['error', {
-      ignores: ['Index', 'About'],
+      ignores: ['Index', 'About', 'Project'],
     }],
     'max-len': ['error', { code: 150, ignoreRegExpLiterals: true }],
     'linebreak-style': 0,
